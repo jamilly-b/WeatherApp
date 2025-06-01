1a7793290c187429ff7e9936767614b8a4a4d515
jamilly-b <jbm2@discente.ifpe.edu.br>
2025-05-31 23:04:50 -0300

    Prática 2 - Parte 1

--- app/src/main/java/com/PDM/weatherapp/MainActivity.kt
+++ app/src/main/java/com/PDM/weatherapp/MainActivity.kt
@@ -1,30 +1,26 @@
 package com.PDM.weatherapp
 
-import android.app.Activity
-import android.content.Intent
-import android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP
 import android.os.Bundle
-import android.text.Layout
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.LineHeightStyle.Alignment.Companion
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.PDM.weatherapp.ui.theme.WeatherAppTheme
-import androidx.compose.ui.Alignment
+import com.PDM.weatherapp.ui.HomePage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,25 +52,3 @@ fun GreetingPreview() {
     }
 }
 
-@Composable
-fun HomePage(modifier: Modifier = Modifier){
-    val activity = LocalContext.current as? Activity
-
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Bem-vindo/a!",
-            fontSize = 24.sp
-        )
-        Button(
-            onClick = {
-                activity?.finish()
-            }
-        ) {
-            Text("Sair")
-        }
-    }
-}
