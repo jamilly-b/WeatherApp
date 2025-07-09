a476831787741957bbbd2759d1f938c5fb6703f8
jamilly-b <jbm2@discente.ifpe.edu.br>
2025-07-08 22:53:54 -0300

    Prática 3 finalizada

--- app/src/main/java/com/PDM/weatherapp/MainActivity.kt
+++ app/src/main/java/com/PDM/weatherapp/MainActivity.kt
@@ -18,9 +18,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.PDM.weatherapp.model.MainViewModel
+import com.PDM.weatherapp.ui.CityDialog
 import com.PDM.weatherapp.ui.nav.BottomNavBar
 import com.PDM.weatherapp.ui.nav.BottomNavItem
 import com.PDM.weatherapp.ui.nav.MainNavHost
@@ -34,8 +39,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             val navController = rememberNavController()
             val viewModel : MainViewModel by viewModels()
+            var showDialog by remember { mutableStateOf(false) }
 
             WeatherAppTheme {
+                if (showDialog) CityDialog(
+                    onDismiss = { showDialog = false },
+                    onConfirm = { city ->
+                        if (city.isNotBlank()) viewModel.add(city)
+                        showDialog = false
+                    }
+                )
                 Scaffold(
                     topBar = {
                         TopAppBar(
@@ -68,7 +81,7 @@ class MainActivity : ComponentActivity() {
 
                     floatingActionButton = {
 
-                        FloatingActionButton(onClick = { }) {
+                        FloatingActionButton(onClick = { showDialog = true }) {
                             Icon(Icons.Default.Add, contentDescription = "Adicionar")
                         }
                     }
