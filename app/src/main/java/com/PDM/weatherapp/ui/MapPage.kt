af6eae9d49e02696adb33b1b8b55e2ef1e91fb29
jamilly-b <jbm2@discente.ifpe.edu.br>
2025-07-13 21:34:49 -0300

    Trabalhando com a localização do usuário

--- app/src/main/java/com/PDM/weatherapp/ui/MapPage.kt
+++ app/src/main/java/com/PDM/weatherapp/ui/MapPage.kt
@@ -1,12 +1,21 @@
 package com.PDM.weatherapp.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import com.PDM.weatherapp.model.MainViewModel
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
@@ -19,8 +28,19 @@ fun MapPage(viewModel: MainViewModel) {
 
     val camPosState = rememberCameraPositionState ()
 
+    val context = LocalContext.current
+    val hasLocationPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(context,
+                Manifest.permission.ACCESS_FINE_LOCATION) ==
+                    PackageManager.PERMISSION_GRANTED
+        )
+    }
+
     GoogleMap( modifier = Modifier.fillMaxSize(), cameraPositionState = camPosState,
-        onMapClick = { viewModel.add("Cidade@${it.latitude}:${it.longitude}", location = it) }
+        onMapClick = { viewModel.add("Cidade@${it.latitude}:${it.longitude}", location = it) },
+        properties = MapProperties(isMyLocationEnabled = hasLocationPermission),
+        uiSettings = MapUiSettings(myLocationButtonEnabled = true)
     )  {
         viewModel.cities.forEach {
             if (it.location != null) {
